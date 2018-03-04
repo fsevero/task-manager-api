64c21e572f8fc53c8dadbe8f434bceb4142d5ab8
Severo <severo.fabricio@gmail.com>
2018-03-04 17:51:41 +0000

    Changes double to single quotes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,9 @@ Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-    # Version 01
-    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show]
     end
-
-    # # Version 02
-    # namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
-    #   resources :tasks
-    # end
   end
 end

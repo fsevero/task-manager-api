a151f22e849cf716c7ef2b6d5323b605da9f4d3d
Severo <severo.fabricio@gmail.com>
2018-03-04 14:44:45 +0000

    Adds ApiVersionConstraint lib

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-    #
+    # Version 01
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      #
+    end
+
+    # # Version 02
+    # namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
+    #   resources :tasks
+    # end
   end
 end

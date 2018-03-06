8688d605ffbe152a47116c15decc260e9b810f62
Severo <severo.fabricio@gmail.com>
2018-03-06 16:25:44 +0000

    Adds authenticate_with_token! in Authenticable

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Authenticable do
 
   let(:app_controller) { subject }
 
+
   describe '#current_user' do
     let(:user) { create(:user) }
 
@@ -18,5 +19,31 @@ RSpec.describe Authenticable do
     it 'returns the user from the authorization header' do
       expect(app_controller.current_user).to eq(user)
     end
+  end
+
+
+  describe '#authenticate_with_token!' do
+    controller do
+      before_action :authenticate_with_token!
+
+      def restricted_action; end
+    end
+
+    context 'when there is no user logged in' do
+      before do
+        allow(app_controller).to receive(:current_user).and_return(nil)
+        routes.draw { get 'restricted_action' => 'anonymous#restricted_action' }
+        get :restricted_action
+      end
+
+      it 'returns status code 401' do
+        expect(response).to have_http_status(401)
+      end
+
+      it 'return json data for the errors' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
+
   end
 end

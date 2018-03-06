ede3d6c5d508a21288c25f5affd24871327e7b59
Severo <severo.fabricio@gmail.com>
2018-03-06 18:19:14 +0000

    Adds tasks@destroy

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -129,4 +129,20 @@ RSpec.describe 'Tasks API' do
       end
     end
   end
+
+  describe 'DELETE /tasks/:id' do
+    let(:task) { create(:task, user_id: user.id) }
+
+    before do
+      delete "/tasks/#{task.id}", params: {}.to_json, headers: headers
+    end
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+
+    it 'removes the tasks from the database' do
+      expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end

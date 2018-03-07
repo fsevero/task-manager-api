4348ed5586a6922e6eb06d93fe1834bd60d74bcf
Severo <severo.fabricio@gmail.com>
2018-03-07 03:35:51 +0000

    Refactors v2 to json api

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'Tasks API' do
     end
 
     it 'returns 5 tasks from database' do
-      expect(json_body[:tasks].count).to eq(5)
+      expect(json_body[:data].count).to eq(5)
     end
   end
 
@@ -37,7 +37,7 @@ RSpec.describe 'Tasks API' do
     end
 
     it 'returns json data for the task' do
-      expect(json_body[:title]).to eq(task.title)
+      expect(json_body[:data][:attributes][:title]).to eq(task.title)
     end
   end
 
@@ -58,11 +58,11 @@ RSpec.describe 'Tasks API' do
       end
 
       it 'returns json data for the task' do
-        expect(json_body[:title]).to eq(task_params[:title])
+        expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
       end
 
       it 'assigns the tasks to the correct user' do
-        expect(json_body[:user_id]).to eq(user.id)
+        expect(json_body[:data][:attributes][:'user-id']).to eq(user.id)
       end
     end
 
@@ -101,7 +101,7 @@ RSpec.describe 'Tasks API' do
       end
 
       it 'returns json data for the updates task' do
-        expect(json_body[:title]).to eq(task_params[:title])
+        expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
       end
 
       it 'updates the task in the database' do
